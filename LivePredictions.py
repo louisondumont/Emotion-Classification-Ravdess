9c5317cde9d3c440e050018db124f51d1f56245a
Louison <>
2019-07-09 02:15:09 -0700

    added server capabilities

--- LivePredictions.py
+++ LivePredictions.py
@@ -20,11 +20,11 @@ class livePredictions:
         self.loaded_model = keras.models.load_model(self.path)
         return self.loaded_model.summary()
 
-    def makepredictions(self):
+    def makepredictions(self, file):
         '''
         I am here to process the files and create your features.
         '''
-        data, sampling_rate = librosa.load(self.file)
+        data, sampling_rate = librosa.load(file)
         mfccs = np.mean(librosa.feature.mfcc(y=data, sr=sampling_rate, n_mfcc=40).T, axis=0)
         x = np.expand_dims(mfccs, axis=2)
         x = np.expand_dims(x, axis=0)
@@ -65,8 +65,55 @@ class livePredictions:
 # Here you can replace path and file with the path of your model and of the file from the RAVDESS dataset you want to use for the prediction,
 # Below, I have used a neutral file: the prediction made is neutral.
 
-pred = livePredictions(path='/Users/marcogdepinto/Desktop/Ravdess_V2/Emotion_Voice_Detection_Model.h5',
+pred = livePredictions(path='./Emotion_Voice_Detection_Model.h5',
                        file='/Users/marcogdepinto/Desktop/Ravdess_V2/01-01-01-01-01-01-01.wav')
 
 pred.load_model()
-pred.makepredictions()
+
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+import simplejson
+
+PORT_NUMBER = 8080
+
+#This class will handles any incoming request from
+#the browser 
+class myHandler(BaseHTTPRequestHandler):
+    
+    #Handler for the GET requests
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        # Send the html message
+        self.wfile.write("Hello World !")
+        return
+
+    def do_POST(self):
+        self._set_headers()
+        print "in post method"
+        self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+
+        self.send_response(200)
+        self.end_headers()
+
+        data = simplejson.loads(self.data_string)
+        self.wfile.write(pred.makepredictions(data["file"]))
+        return
+
+
+try:
+    #Create a web server and define the handler to manage the
+    #incoming request
+    server = HTTPServer(('', PORT_NUMBER), myHandler)
+    print 'Started httpserver on port ' , PORT_NUMBER
+    
+    #Wait forever for incoming htto requests
+    server.serve_forever()
+
+except KeyboardInterrupt:
+    print '^C received, shutting down the web server'
+    server.socket.close()
+
+
+
+
